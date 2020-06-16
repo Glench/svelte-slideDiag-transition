022af69038595806383d799ef7e7cf27be8b4bae
Glen Chiacchieri <www@glench.com>
2020-06-16 14:42:06 -0400

    transition function

---
+++ index.js
@@ -0,0 +1,41 @@
+export function slideDiag(node, options={
+    delay : 0,
+    duration : 400,
+    easing : cubicOut
+}) {
+    const style = getComputedStyle(node);
+    const opacity = +style.opacity;
+    const height = parseFloat(style.height);
+    const width = parseFloat(style.width);
+    const padding_top = parseFloat(style.paddingTop);
+    const padding_bottom = parseFloat(style.paddingBottom);
+    const padding_left = parseFloat(style.paddingLeft);
+    const padding_right = parseFloat(style.paddingRight);
+    const margin_top = parseFloat(style.marginTop);
+    const margin_bottom = parseFloat(style.marginBottom);
+    const margin_left = parseFloat(style.marginLeft);
+    const margin_right = parseFloat(style.marginRight);
+    const border_top_width = parseFloat(style.borderTopWidth);
+    const border_bottom_width = parseFloat(style.borderBottomWidth);
+    const border_left_width = parseFloat(style.borderLeftWidth);
+    const border_right_width = parseFloat(style.borderRightWidth);
+
+    return {
+        delay: options.delay,
+        duration: options.duration,
+        easing: options.easing,
+        css: t =>
+            `overflow: hidden;` +
+            `opacity: ${Math.min(t * 20, 1) * opacity};` +
+            `height: ${t * height}px;` +
+            `width: ${t * width}px;` +
+            `padding-top: ${t * padding_top}px;` +
+            `padding-bottom: ${t * padding_bottom}px;` +
+            `margin-top: ${t * margin_top}px;` +
+            `margin-bottom: ${t * margin_bottom}px;` +
+            `border-top-width: ${t * border_top_width}px;` +
+            `border-bottom-width: ${t * border_bottom_width}px;`
+    };
+}
+
+
